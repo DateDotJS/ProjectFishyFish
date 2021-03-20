c52daedbeaed0b3c9762070fc2f4cb108240f0e0
Elizabeth Lam <animew.cccl@gmail.com>
2021-03-19 22:43:57 -0400

    Set up flock

---
+++ Assets/Scripts/FlockAgent.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class FlockAgent : MonoBehaviour
+{
+    public Collider AgentCollider { get; set; }
+
+    void Start()
+    {
+        AgentCollider = GetComponent<Collider>();
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        transform.forward = velocity;
+        transform.position += velocity * Time.deltaTime;
+    }
+}
