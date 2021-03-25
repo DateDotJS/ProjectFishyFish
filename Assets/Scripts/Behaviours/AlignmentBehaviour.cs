c367eca45d07a303118b9722b1f4ee8b189c0c41
Elizabeth Lam <animew.cccl@gmail.com>
2021-03-24 20:30:31 -0400

    Remove code redundancy

--- Assets/Scripts/Behaviours/AlignmentBehaviour.cs
+++ Assets/Scripts/Behaviours/AlignmentBehaviour.cs
@@ -5,19 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Flock/Behaviour/Alignment")]
 public class AlignmentBehaviour : FilteredFlockBehaviour
 {
-    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    public override Vector3 CalculateFilteredMove(FlockAgent agent, List<Transform> context, Flock flock)
     {
-        List<Transform> filteredContext = (filter is null) ? context : filter.Filter(agent, context);
-        if (filteredContext.Count == 0)
+        if (context.Count == 0)
             return agent.transform.forward;
 
         Vector3 alignmentMove = Vector3.zero;
-        foreach (Transform item in filteredContext)
+        foreach (Transform item in context)
         {
             alignmentMove += item.transform.forward;
         }
 
-        alignmentMove /= filteredContext.Count;
+        alignmentMove /= context.Count;
 
         return alignmentMove;
     }
