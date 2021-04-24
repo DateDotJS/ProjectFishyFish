a22a6844128d23aac6a5eee85327d767a8dd9480
Shifat Khan <shifatkhan6@gmail.com>
2021-04-24 19:35:23 -0400

    Add split behaviour (#38)

---
+++ Assets/Scripts/Flocking/Behaviours/SplitBehaviour.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behaviour/Split Behaviour")]
+public class SplitBehaviour : FilteredFlockBehaviour
+{
+    private bool speedChanged = false;
+    public float ExtraSpeed = 2.4f;
+    public float SplitDistance = 5f;
+    
+    public override Vector3 CalculateFilteredMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        //No predator in sight
+        if (agent.PredatorList.Count == 0)
+        {
+            //Reset speed to initial one
+            if (speedChanged == true)
+            {
+                flock.ChangeFlockSpeed(flock.GetFlockSpeed() / ExtraSpeed);
+                speedChanged = false;
+            }
+
+            return Vector3.zero;
+        }
+
+        var splitDirection = Vector3.zero;
+        var nThingsToAvoid = agent.PredatorList.Count;
+        var agentPosition = agent.transform.position;
+        var avgPredatorPos = Vector3.zero;
+        var avgPredatorRight = Vector3.zero;
+        foreach (Transform predator in agent.PredatorList)
+        {
+            Vector3 closestPoint = predator.gameObject.GetComponent<Collider>().ClosestPoint(agentPosition);
+            
+            avgPredatorPos += closestPoint; //Gets average position of predators
+            avgPredatorRight += predator.transform.right;
+        }
+        avgPredatorPos /= nThingsToAvoid;
+        avgPredatorRight /= nThingsToAvoid;
+        
+        if (speedChanged == false)
+        {
+            flock.ChangeFlockSpeed(flock.GetFlockSpeed() * ExtraSpeed);
+            speedChanged = true;
+        }
+        
+        // Split direction.
+        Vector3 distance = agentPosition - avgPredatorPos;
+        var side = Vector3.Dot(distance, avgPredatorRight);
+        splitDirection = avgPredatorRight * ((side < 0 ? -1 : 1) * SplitDistance);
+        
+        return splitDirection;
+    }
+}
