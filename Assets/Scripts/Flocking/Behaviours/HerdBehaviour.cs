24b782181567188d539e98bc28e58ef3a4ec5a3e
Elizabeth Lam <animew.cccl@gmail.com>
2021-04-28 03:57:48 -0400

    Update states and fix herd bug

--- Assets/Scripts/Flocking/Behaviours/HerdBehaviour.cs
+++ Assets/Scripts/Flocking/Behaviours/HerdBehaviour.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,33 +11,21 @@ public class HerdBehaviour : FilteredFlockBehaviour
     {
         //No predator in sight
         if (agent.PredatorList.Count == 0)
-        {
-            //Reset speed to initial one
-            if (speedChanged == true)
-            {
-                //flock.ChangeFlockSpeed(flock.GetFlockSpeed() / speedFactor);
-                speedChanged = false;
-            }
-
             return Vector3.zero;
-        }
 
         Vector3 herdMove = Vector3.zero;
+        Vector3 agentPosition = agent.transform.position;
 
         Transform predator = agent.PredatorList[0].transform;
         float angle = Vector3.SignedAngle(predator.forward, agent.transform.position - predator.position, Vector3.up);
-        
-       if ( (angle > 0 && angle < 45) || (angle < 0 && angle > -45) )
-            if (speedChanged == false)
-            {
-                //flock.ChangeFlockSpeed(flock.GetFlockSpeed() * speedFactor);
-                speedChanged = true;
-            }
+
+        if ((angle > 0 && angle < 45) || (angle < 0 && angle > -45))
+            herdMove =  agentPosition - (-predator.transform.position) * speedFactor;
         else if (angle > 45 && angle < 90)
-                herdMove = agent.transform.position + agent.transform.right * Time.deltaTime * 0.3f;
+            herdMove = agentPosition + agent.transform.right;
         else if (angle < -45 && angle > -90)
-                herdMove = agent.transform.position - agent.transform.right * Time.deltaTime * 0.3f;
-        
-        return herdMove * (speedChanged ? speedFactor : 1);
+            herdMove = agentPosition - agent.transform.right;
+
+        return herdMove;
     }
 }
